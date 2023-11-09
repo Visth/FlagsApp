0324facce2bc49c99c4ca467b3b07f5b3d1fe820
Filip <filber43@gmail.com>
2023-11-09 16:42:20 +0100

    Created API menagement and basic display of the data

---
+++ dom-utils.js
@@ -0,0 +1,59 @@
+const createInfoElement = (labelName, value) => {
+	const infoElement = document.createElement("div");
+
+	const labelElement = document.createElement("strong");
+	labelElement.innerText = `${labelName}: `;
+
+	const valueElement = document.createElement("span");
+	valueElement.innerText = value;
+
+	infoElement.appendChild(labelElement);
+	infoElement.appendChild(valueElement);
+
+	return infoElement;
+};
+
+const createFlagImgElement = (country) => {
+	const imgContaierElement = document.createElement("div");
+	const imgElement = document.createElement("img");
+	imgElement.src = country.flagUrl;
+	imgElement.width = 160;
+	imgElement.height = 98;
+
+	imgContaierElement.appendChild(imgElement);
+
+    return imgContaierElement;
+};
+
+const createCountryItemElement = (country) => {
+	const countryElement = document.createElement("li");
+
+	const countryNameElement = document.createElement("span");
+	countryNameElement.innerText = country.name;
+
+    countryElement.appendChild(createFlagImgElement(country));
+
+	countryElement.appendChild(countryNameElement);
+
+	countryElement.appendChild(createInfoElement("Population", country.population));
+
+	countryElement.appendChild(createInfoElement("Region", country.region));
+
+	countryElement.appendChild(createInfoElement("Capital", country.capital));
+
+	return countryElement;
+};
+
+const createListElement = (countries) => {
+	const listElement = document.createElement("ul");
+	countries.forEach((country) => {
+		listElement.appendChild(createCountryItemElement(country));
+	});
+
+	return listElement;
+};
+
+export const renderCountriesList = (countries) => {
+	const rootElement = document.querySelector("#root");
+	rootElement.appendChild(createListElement(countries));
+};
